d6db23be2f13f6093a6fdc79c9b9d52cb412e8d6
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-31 21:40:23 -0700

    set up slider

--- app/src/main/java/com/example/clarkhinchcliff/beareggtimer/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/beareggtimer/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.clarkhinchcliff.beareggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.suitebuilder.TestMethod;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SeekBar timerSeekbar = (SeekBar)findViewById(R.id.timerSeekBar);
+        final TextView timerTextview = (TextView)findViewById(R.id.textView);
+
+        timerSeekbar.setMax(600);
+        timerSeekbar.setProgress(30);
+
+        timerSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes = (int) progress/60;
+                int seconds = (int) progress - minutes * 60;
+
+                timerTextview.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
